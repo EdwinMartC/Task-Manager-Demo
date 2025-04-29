e8fc0d96173341c01b9b055f5a9833b775d15cff
Edwin Mart <edwinmart29@gmail.com>
2025-04-28 21:31:03 -0500

    Third Commit. Added classes and interfaces for creating tasks

---
+++ src/main/java/com/example/demo/mapper/TaskInDTOToTask.java
@@ -0,0 +1,23 @@
+package com.example.demo.mapper;
+
+import com.example.demo.persistence.Entity.Task;
+import com.example.demo.persistence.Entity.TaskStatus;
+import com.example.demo.service.dto.TaskInDTO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class TaskInDTOToTask implements IMapper<TaskInDTO, Task>{
+    @Override
+    public Task map(TaskInDTO in) {
+        Task task = new Task();
+        task.setTitle(in.getTitle());
+        task.setDescription(in.getDescription());
+        task.setEta(in.getEta());
+        task.setCreateDate(LocalDateTime.now());
+        task.setFinished(false);
+        task.setTaskstatus(TaskStatus.ON_TIME);
+        return task;
+    }
+}
--- src/main/java/com/example/demo/service/TaskService.java
+++ src/main/java/com/example/demo/service/TaskService.java
@@ -1,14 +1,24 @@
 package com.example.demo.service;
 
+import com.example.demo.mapper.TaskInDTOToTask;
+import com.example.demo.persistence.Entity.Task;
 import com.example.demo.persistence.repository.TaskRepository;
+import com.example.demo.service.dto.TaskInDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
 
     private final TaskRepository repository;
+    private final TaskInDTOToTask mapperTask;
 
-    public TaskService(TaskRepository repository) {
+    public TaskService(TaskRepository repository, TaskInDTOToTask mapperTask) {
         this.repository = repository;
+        this.mapperTask = mapperTask;
+    }
+
+    public Task createTask(TaskInDTO taskInDTO){
+        Task task = mapperTask.map(taskInDTO);
+        return this.repository.save(task);
     }
 }
